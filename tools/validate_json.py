761b213da55d9a65d84fefddd0fe4202de8862ad
thibault.napoleon@gmail.com <thibault.napoleon@isen-ouest.yncrea.fr>
2025-11-19 22:20:01 +0100

    JSON validation tool in Python added

---
+++ tools/validate_json.py
@@ -0,0 +1,72 @@
+#!/home/napoleon/.PythonEnvs/Base/bin/python3
+###
+# \\Author: Thibault Napoléon "Imothep"
+# \\Company: ISEN Ouest
+# \\Email: thibault.napoleon@isen-ouest.yncrea.fr
+# \\Created Date: 19-Nov-2025 - 18:49:30
+# \\Last Modified: 19-Nov-2025 - 21:46:11
+###
+
+"""JSON validator script."""
+
+# Imports.
+import json
+import jsonschema
+import argparse
+import os
+
+
+# Constants.
+DATA = {
+    'Bio':
+        ['../json/bio.json', 'schemas/bio_schema.json'],
+    'Profil':
+        ['../json/profil.json', 'schemas/profil_schema.json'],
+    'Publications':
+        ['../json/publications.json', 'schemas/publications_schema.json'],
+    'Research':
+        ['../json/research.json', 'schemas/research_schema.json'],
+    'Teaching':
+        ['../json/teaching.json', 'schemas/teaching_schema.json']
+}
+OK = '\033[92m'
+FAIL = '\033[91m'
+
+
+def loadJSON(json_file):
+    """Load and parse a JSON file."""
+    if not os.path.exists(json_file):
+        return False, FAIL + 'The JSON file ' + json_file + ' doesn\'t exists.'
+    try:
+       json_data = json.load(open(json_file))
+    except json.decoder.JSONDecodeError:
+        return False, FAIL + 'Unable to parse the JSON file ' + json_file
+    return True, json_data
+
+
+def validateJSON(json_file, schema_file):
+    """Validate a JSON file accordingly to a JSON schema."""
+    state, json_data = loadJSON(json_file)
+    if not state:
+        return False, json_data
+    state, json_schema = loadJSON(schema_file)
+    if not state:
+        return False, json_schema
+
+    # Validate the JSON file.
+    try:
+        jsonschema.validate(instance=json_data, schema=json_schema)
+    except jsonschema.ValidationError as e:
+        return False, (FAIL + 'JSON file' + json_file +
+                       ' is not valid accordingly to ' + schema_file + ': '+
+                       e.message)
+    return True, None
+
+
+# Main program.
+for key in DATA:
+    state, message = validateJSON(*DATA[key])
+    if state:
+        print(OK +  key + ' OK')
+    else:
+        print(FAIL + key + ' KO (' + message + ')')
